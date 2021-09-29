998451a8cac104c8dc491daf791a3f825b04261f
sahana178 <85974914+sahana178@users.noreply.github.com>
2021-09-28 21:06:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var name_student_array=[];
+function submit(){
+var name_1=document.getElementById("student_1").value;
+var name_2=document.getElementById("student_2").value;
+var name_3=document.getElementById("student_3").value;
+var name_4=document.getElementById("student_4").value;
+
+name_student_array.push(name_1);
+name_student_array.push(name_2);
+name_student_array.push(name_3);
+name_student_array.push(name_4);
+
+console.log(name_student_array);
+document.getElementById("display").innerHTML=name_student_array;
+
+document.getElementById("submit_button").style.display="none";
+document.getElementById("sort_button").style.display="inline-block";
+}
+
+function sorting(){
+    name_student_array.sort();
+    console.log(name_student_array);
+    document.getElementById("display").innerHTML=name_student_array;
+}
